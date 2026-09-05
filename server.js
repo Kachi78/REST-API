5766361bab408272689856614dbbc2b3fac1df02
Kachi78 <chukwuemekaonyekachi06@gmail.com>
2026-09-05 15:46:10 +0100

    Pushing codes

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoute');
 
 const atlasString = "mongodb+srv://chukwuemekaonyekachi06_db_user:AZS6sVgl4xoYJoO6@cluster0.xhpkgpt.mongodb.net/kachi_81?appName=Cluster0";
 
-mongoose.connect(atlasString).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log('Connected to MongoDB Atlas');
 }).catch((error) => {
     console.error('Error connecting to MongoDb Atlas:', error);
 });
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
