9a65358f45d7ebc4f298cd6522ad234c28b8ec18
Kachi78 <chukwuemekaonyekachi06@gmail.com>
2026-09-05 15:32:47 +0100

    Pushing codes

---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const userRoute = express.Router();
+const { createUser, getSingleUser, updateUser, deleteUser } = require('../controller/userController');
+
+userRoute.post('/new-user', createUser);
+userRoute.get('/single-user/:userId', getSingleUser);
+userRoute.patch('/update-user/:userId', updateUser);
+userRoute.delete('/delete-user/:userId', deleteUser);
+
+module.exports = userRoute;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoute');
+
+const atlasString = "mongodb+srv://chukwuemekaonyekachi06_db_user:AZS6sVgl4xoYJoO6@cluster0.xhpkgpt.mongodb.net/kachi_81?appName=Cluster0";
+
+mongoose.connect(atlasString).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((error) => {
+    console.error('Error connecting to MongoDb Atlas:', error);
+});
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
